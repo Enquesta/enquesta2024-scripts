6473af96a5d373f8bfae78b00a32847f95df8bea
AllenJosephM <glitchless787@gmail.com>
2024-07-05 12:17:24 +0530

    Round 2-questions complete

--- Finals/Round 2/reveal.js-master/main.py
+++ Finals/Round 2/reveal.js-master/main.py
@@ -3,7 +3,6 @@ import os
 # File Paths
 input_questions = 'questions.txt'
 input_answers = 'answers.txt'
-input_topics = 'topics.txt'
 output_file = 'main1.html'
 
 # The main html stuff
@@ -14,7 +13,7 @@ html_content = """
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-		<title>Enquesta-Round 2-Bidding War</title>
+		<title>Enquesta-Round 1-Pounce and Bounce</title>
 
 		<link rel="stylesheet" href="dist/reset.css">
 		<link rel="stylesheet" href="dist/reveal.css">
@@ -22,44 +21,49 @@ html_content = """
 
 		<!-- Theme used for syntax highlighted code -->
 		<link rel="stylesheet" href="plugin/highlight/monokai.css">
-		<style>
-            .horizontal-links a {
-                display: inline;
-                margin-right: 10px; /* Optional: Adds space between links */
+    <style>
+        .image-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
         }
-        .disabled-link {
-            pointer-events: none;
-            color: grey;
-            text-decoration: none;
+        .image-container img {
+            max-width: 100%;
+            max-height: 100%;
+            object-fit: contain;
+            box-sizing: border-box;
         }
-        </style>
+    </style>
 	</head>
 
 	<body>
 		<div class="reveal">
 			<div class="slides">
 """
-#Reading Topics
-with open(input_topics, 'r') as file:
-    topics = file.readlines()
 
-# Reading Questions
-rows1, cols1 = len(topics), 3
-questions = [["" for _ in range(cols1)] for _ in range(rows1)]
-x1 = -1
+#Reading Questions
 z1 = -1
+questions = ["" for _ in range(18)]
 with open(input_questions, 'r') as file:
-    lines = file.readlines()
-    for y in range(0,len(lines)):
-        lines[y].strip()
-        if lines[y].startswith('#'):
-            x1 += 1
-            z1 = -1
-        else:
-            if(lines[y].startswith('-')):
-                z1 += 1
-            questions[x1][z1] += lines[y]
-            questions[x1][z1].replace('-','')
+    lines1 = file.readlines()
+    for i in range(len(lines1)):
+        if (lines1[i].startswith('-')):
+            z1 += 1
+            questions[z1] += lines1[i]
+
+
+#Reading Answers
+z2 = -1
+answers = ["" for _ in range(18)]
+with open(input_answers, 'r') as file:
+    lines2 = file.readlines()
+    for i in range(len(lines2)):
+        if (lines2[i].startswith('-')):
+            z2 += 1
+            answers[z2] += lines2[i]
 
 #Reading images
 questions_with_images = -1
@@ -73,58 +77,17 @@ for i in range(0, len(questions)-1):
 print(images)
 print(n_images)
 
-# Reading Answers
-rows2,cols2=len(topics), 4
-answers = [["empty" for _ in range(cols2)] for _ in range(rows2)]
-x2 = -1
-z2 = 0
-with open(input_answers, 'r') as file:
-    lines = file.readlines()
-    for y in range(0,len(lines)):
-        if lines[y].startswith('#'):
-            x2 += 1
-            z2 = 0
-        else:
-            answers[x2][z2] = lines[y]
-            z2 += 1
+#Adding Questions, Answers, Images
+for i in range(len(questions)):
+    html_content += '<section>'
+    html_content += '<section><h4>Question ' +str(i+1)+'</h4><p class="r-fit-text">'+questions[i]+'<p></section>\n'
+    if i in images:
+        html_content += '<section data-background="images/image'+str(i)+'-0.png" data-background-size="contain">\n'
 
-
-#Adding Topics
-html_content += '<section id=0>\n'
-html_content += '<div class="horizontal-links">\n'
-for x in range(0, len(topics)):
-    html_content += '<a href="#/' + topics[x] + '" class="one-time-link">' + topics[x] + '</a>\n'
-    if (x == len(topics)/2):
-        html_content += '</div>\n<div class="horizontal-links">\n'
-html_content += '</div>\n'
-html_content += '</section>\n'
-
-
-# Adding Questions, Answers and Header Slides
-for x in range(0, len(topics)):
-    html_content += '<section id="'+topics[x]+'">\n<h1>'+topics[x]+'</h1>'
-
-    html_content += '<div class="horizontal-links">\n'
-    for y in range(0, len(questions[x])):
-        html_content += '<a href="#/' + topics[x] + str(30-y*10) + str(x)+'">' + str(30-y*10) + '</a>\n'
-    html_content += '</div>\n'
-    html_content += '</section>\n'
-
-    for y in range(0, len(questions[x])):
-        html_content += '<section id="'+ topics[x] + str(30-y*10)+str(x)+'">\n'
-        html_content += '<section><p class=""r-fit-text">' + questions[x][y] + '</p></section>\n'
-        if y in images:
-            html_content += '<section data-background=images/image' + str(
-                y) + '-0.png data-background-size="contain">\n'
-
-            html_content += '</section>\n'
-        html_content += '<section data-background-image="memes/meme' + str(x) + (
-            '.png" data-background-size="contain" data-background-position="center"></section>\n')
-        html_content += '<section><p>' + answers[x][y]
-        html_content += '</p><a href="#/0">Back to Topics</a>' + '</section>\n'
-        #print(questions[x][y])
         html_content += '</section>\n'
-    html_content += '<section><a href="#/0">Back to Topics</a></section>'
+    html_content += '<section data-background="memes/meme0.png"></section\n>'
+    html_content += '<section>' + answers[i] + '</section>\n'
+    html_content += '</section>\n'
 
 # Closing html tags
 html_content += """			
@@ -145,19 +108,26 @@ html_content += """
 				// Learn about plugins: https://revealjs.com/plugins/
 				plugins: [ RevealMarkdown, RevealHighlight, RevealNotes ]
 			});
-        document.addEventListener('DOMContentLoaded', () => {
-            const links = document.querySelectorAll('.one-time-link');
-            
-            links.forEach(link => {
-                link.addEventListener('click', (event) => {
-                    // Allow the link to function once
-                    setTimeout(() => {
-                        link.classList.add('disabled-link');
-                        link.removeAttribute('href');
-                    }, 100);
-                });
-            });
-        });
+			document.addEventListener('DOMContentLoaded', () => {
+    function updateImageSize() {
+        const imageContainer = document.querySelector('.image-container');
+        const image = imageContainer.querySelector('img');
+        const containerWidth = imageContainer.offsetWidth;
+        const containerHeight = imageContainer.offsetHeight;
+
+        // Ensure image fits within the container
+        if (image.naturalWidth > containerWidth || image.naturalHeight > containerHeight) {
+            image.style.maxWidth = `${containerWidth}px`;
+            image.style.maxHeight = `${containerHeight}px`;
+        } else {
+            image.style.maxWidth = '100%';
+            image.style.maxHeight = '100%';
+        }
+    }
+
+    updateImageSize();
+    window.addEventListener('resize', updateImageSize);
+});
 		</script>
 	</body>
 </html>
